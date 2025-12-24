a5176d271eb9a0e892236103534efad05ff05a9e
Aghlimi <ahmed4aghlimi@gmail.com>
2025-12-25 00:39:45 +0100

    fix database permission

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -5,5 +5,6 @@ cd /var/www/html
 
 php artisan migrate --force
 php artisan db:seed --force || true
-
+# give chmod 777 to database.sqlite
+chmod 777 database/database.sqlite
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
