63cb77a4a50ab55fa42572df43101a185cfbc201
Aghlimi <ahmed4aghlimi@gmail.com>
2025-12-24 18:49:20 +0100

    fix seeder

--- backend/entry-point.sh
+++ backend/entry-point.sh
@@ -4,6 +4,8 @@ set -e
 
 php artisan migrate --force
 
+composer dump-autoload
+
 php artisan db:seed --force
 
-php artisan serve --host=0.0.0.0 --port=80
+php artisan serve --host=0.0.0.0 --port=8000
